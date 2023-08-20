6cf1c412734454459278dd959d931f5e3c84e749
niroj neupane <knirojneupane@gmail.com>
2023-08-20 13:30:15 +0545

    Complete js for game function

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-function getComputerChoice(){
-    let choice=1;
-    switch(choice){
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
         case 0:
             return "ROCK";
             break;
         case 1:
-            return "Paper";
+            return "PAPER";
             break;
         case 2:
-            return "SCISSORS";
+            return "scissor";
             break;
     }
 }
-function playRound(playerSelection,computerSelection){
-    if(playerSelection.localeCompare(computerSelection)==0)
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "ROCK" && computerSelection == "ROCK" ||
+        playerSelection == "PAPER" && computerSelection == "PAPER" ||
+        playerSelection == "SCISSOR" && computerSelection == "SCISSOR")
         return "TIE"
-    else if(playerSelection=="ROCK"){
-        if(computerSelection=="PAPER"){
-            return "YOU LOSE";
+    else if (playerSelection == "ROCK") {
+        if (computerSelection == "PAPER") {
+            return "YOU LOSE.PAPER beats ROCK";
         }
-        else{
+        else {
             return "YOU WIN";
         }
     }
-    else if(playerSelection=="PAPER"){
-        if(computerSelection=="SCISSORS"){
-            return "YOU LOSE";
+    else if (playerSelection == "PAPER") {
+        if (computerSelection == "SCISSOR") {
+            return "YOU LOSE scissor beats PAPER";
         }
-        else{
+        else {
             return "YOU WIN";
         }
     }
-    else if(playerSelection=="SCISSORS"){
-        if(computerSelection=="ROCK"){
-            return "YOU LOSE";
+    else if (playerSelection == "SCISSOR") {
+        if (computerSelection == "ROCK") {
+            return "YOU LOSE. ROCK beats SCISSOR";
         }
-        else{
+        else {
             return "YOU WIN";
         }
     }
 }
+function game() {
+    let score=0;
+    for (i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("ROCK,PAPERS, or SCISSOR?");
+        playerSelection = playerSelection.toUpperCase();
+        let result = playRound(playerSelection, computerSelection);
+        if(result=="YOU WIN"){
+            score++;
+        }
+        console.log(result);
+        console.log("score= ",score);
+    }
+
+}
+
+
